b264b9695670eb03ccd2617971df9e133979a1bf
wormius51 <eyalhazor1@gmail.com>
2020-09-27 16:52:02 +0300

    match manager

--- Assets/scripts/Match/MatchManager.cs
+++ Assets/scripts/Match/MatchManager.cs
@@ -9,6 +9,16 @@ using UnityEngine;
 public class MatchManager : MonoBehaviour
 {
     public Player[] players;
+
+    ///<summary>
+    /// The nuber of rounds that passed since the start of the match.
+    ///</summary>
+    public int roundNumber {get; private set;}
+
+    ///<summary>
+    /// The index of the player who has the right to move.
+    ///</suammary>
+    public int turnPlayerIndex {get; private set;}
     // Start is called before the first frame update
     void Start()
     {
@@ -19,5 +29,27 @@ public class MatchManager : MonoBehaviour
     void Update()
     {
         
+    }
+
+    ///<summary>
+    /// Passes the turn to the next play that is not eliminated.
+    /// Increments the roundNumber after all players has passed (or made) their turn.
+    ///</summary>
+    /// <returns> True if a living player was found, 
+    /// false if otherwise (everyone is dead).
+    /// </returns>
+    public bool passTurn() {
+        int iterrationTimes = 0;
+        do {
+            if (iterrationTimes >= players.Length)
+                return false;
+            turnPlayerIndex++;
+            if (turnPlayerIndex >= players.Length) {
+                turnPlayerIndex = 0;
+                roundNumber++;
+            }
+            iterrationTimes++;
+        } while (players[turnPlayerIndex].isEliminated);
+        return true;
     }
 }
--- Assets/scripts/Match/Player.cs
+++ Assets/scripts/Match/Player.cs
@@ -2,8 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
 public class Player
 {
     public string name;
     public bool isAI;
+    [HideInInspector]
+    public bool isEliminated;
 }

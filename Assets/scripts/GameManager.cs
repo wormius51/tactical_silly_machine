b10475932f456a01f612c030e782ad2b30929d82
wormius51 <eyalhazor1@gmail.com>
2020-09-27 16:25:15 +0300

    scenes and managers

---
+++ Assets/scripts/GameManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance {get; private set;}
+
+    public GameMode gameMode;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (instance != null) {
+            Destroy(gameObject);
+            return;
+        }
+        DontDestroyOnLoad(gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
+public enum GameMode {
+    PVP, VS_AI, tutorial
+}
---
+++ Assets/scripts/Match/MatchManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+///<summary>
+/// Holds the information about the players and the state of the match.
+/// Information such as: Who's turn is it? How manny players and AI are playing?
+///</summary>
+public class MatchManager : MonoBehaviour
+{
+    public Player[] players;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
